661f9090b4b509efe4457be495773b67d6ec6880
sebas-montilla <sebastianam127@gmail.com>
2022-09-05 15:47:07 -0400

    login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 // import ErrorNotFound from "./components/ErrorNotFound";
 import Register from "./components/Register.jsx";
+import Login from "./components/Login.jsx";
 
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
           <Switch>
             <Route exact path="/register">
               <Register />
+            </Route>
+            <Route exact path="/login">
+              <Login />
             </Route>
             {/* <Route component={ErrorNotFound} /> */}
           </Switch>
