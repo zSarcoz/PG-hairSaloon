90b02a2d1c693bd499b3828ee46e2a7c453d5f5a
sebas-montilla <sebastianam127@gmail.com>
2022-09-09 12:42:32 -0400

    Register and input

--- client/src/components/Forms/Register.jsx
+++ client/src/components/Forms/Register.jsx
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { registerUser, getUsers, currentUser } from "../../redux/actions";
+import Input from "./Input";
 // import { Container, Row, Col } from "react-bootstrap";
 // import "./styles/style.css";
 import "../styles/style.css";
@@ -12,77 +13,77 @@ import "../styles/responsive.css";
 // import bg from "../assets/4.png";
 import bg from "../../assets/4.png";
 
-function validation({
-  name,
-  hp,
-  attack,
-  defense,
-  speed,
-  weight,
-  height,
-  type,
-}) {
-  const errors = {};
+// function validation({
+//   name,
+//   hp,
+//   attack,
+//   defense,
+//   speed,
+//   weight,
+//   height,
+//   type,
+// }) {
+//   const errors = {};
 
-  //validating name
-  if (!name) {
-    errors.name = "Enter Name ❌";
-  } else if (/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(name)) {
-    errors.name = "Characters are not allowed ❌";
-  }
+//   //validating name
+//   if (!name) {
+//     errors.name = "Enter Name ❌";
+//   } else if (/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(name)) {
+//     errors.name = "Characters are not allowed ❌";
+//   }
 
-  //validating hp
-  if (!hp || hp < 10 || hp > 200) {
-    if (!hp) errors.hp = "Enter hp ❌";
-    else if (hp <= 10) errors.hp = "hp must be higher than 10 ❌";
-    else if (hp >= 200) errors.hp = "hp must be lower than 200 ❌";
-  }
+//   //validating hp
+//   if (!hp || hp < 10 || hp > 200) {
+//     if (!hp) errors.hp = "Enter hp ❌";
+//     else if (hp <= 10) errors.hp = "hp must be higher than 10 ❌";
+//     else if (hp >= 200) errors.hp = "hp must be lower than 200 ❌";
+//   }
 
-  //validating attack
-  if (!attack || attack < 10 || attack > 200) {
-    if (!attack) errors.hp = "Enter attack ❌";
-    else if (attack <= 10) errors.attack = "attack must be higher than 10 ❌";
-    else if (attack >= 200) errors.attack = "attack must be lower than 200 ❌";
-  }
+//   //validating attack
+//   if (!attack || attack < 10 || attack > 200) {
+//     if (!attack) errors.hp = "Enter attack ❌";
+//     else if (attack <= 10) errors.attack = "attack must be higher than 10 ❌";
+//     else if (attack >= 200) errors.attack = "attack must be lower than 200 ❌";
+//   }
 
-  //validating defense
-  if (!defense || defense < 10 || defense > 200) {
-    if (!defense) errors.defense = "Enter defense ❌";
-    else if (defense <= 10)
-      errors.defense = "defense must be higher than 10 ❌";
-    else if (defense >= 200)
-      errors.defense = "defense must be lower than 200 ❌";
-  }
+//   //validating defense
+//   if (!defense || defense < 10 || defense > 200) {
+//     if (!defense) errors.defense = "Enter defense ❌";
+//     else if (defense <= 10)
+//       errors.defense = "defense must be higher than 10 ❌";
+//     else if (defense >= 200)
+//       errors.defense = "defense must be lower than 200 ❌";
+//   }
 
-  //validating Speed
-  if (!speed || speed < 10 || speed > 200) {
-    if (!speed) errors.speed = "Enter speed ❌";
-    else if (speed <= 10) errors.speed = "speed must be higher than 10 ❌";
-    else if (speed >= 200) errors.speed = "speed must be lower than 200 ❌";
-  }
+//   //validating Speed
+//   if (!speed || speed < 10 || speed > 200) {
+//     if (!speed) errors.speed = "Enter speed ❌";
+//     else if (speed <= 10) errors.speed = "speed must be higher than 10 ❌";
+//     else if (speed >= 200) errors.speed = "speed must be lower than 200 ❌";
+//   }
 
-  //validating weight
-  if (!weight || weight < 10 || weight > 200) {
-    if (!weight) errors.weight = "Enter weight ❌";
-    else if (weight <= 10) errors.weight = "weight must be higher than 10 ❌";
-    else if (weight >= 200) errors.weight = "weight must be lower than 200 ❌";
-  }
+//   //validating weight
+//   if (!weight || weight < 10 || weight > 200) {
+//     if (!weight) errors.weight = "Enter weight ❌";
+//     else if (weight <= 10) errors.weight = "weight must be higher than 10 ❌";
+//     else if (weight >= 200) errors.weight = "weight must be lower than 200 ❌";
+//   }
 
-  //validating height
-  if (!height || height < 10 || height > 200) {
-    if (!height) errors.height = "Enter height ❌";
-    else if (height <= 10) errors.height = "height must be higher than 10 ❌";
-    else if (height >= 200) errors.height = "height must be lower than 200 ❌";
-  }
+//   //validating height
+//   if (!height || height < 10 || height > 200) {
+//     if (!height) errors.height = "Enter height ❌";
+//     else if (height <= 10) errors.height = "height must be higher than 10 ❌";
+//     else if (height >= 200) errors.height = "height must be lower than 200 ❌";
+//   }
 
-  //validating types
-  if (!type.length) {
-    errors.type = "Must choose a type ❌";
-  } else if (type.length > 2) {
-    errors.type = "You can only select two types ❌";
-  }
-  return errors;
-}
+//   //validating types
+//   if (!type.length) {
+//     errors.type = "Must choose a type ❌";
+//   } else if (type.length > 2) {
+//     errors.type = "You can only select two types ❌";
+//   }
+//   return errors;
+// }
 
 export default function Register() {
   const error = useSelector((state) => state.error);
@@ -90,77 +91,141 @@ export default function Register() {
   const dispatch = useDispatch();
   const [errors, setErrors] = useState({});
 
-  const [user, setUser] = useState({ 
-    name: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    direction: "",
-    cedula: "",
-  });
-  const { name, lastName, email, phone, direction, cedula } = user;
+  const [name, setName] = useState({ value: "", valid: null });
+  const [lastName, setLastName] = useState({ value: "", valid: null });
+  const [phone, setPhone] = useState({ value: "", valid: null });
+  const [email, setEmail] = useState({ value: "", valid: null });
+  const [cedula, setCedula] = useState({ value: "", valid: null });
+  const [direction, setDirection] = useState({ value: "", valid: null });
+  console.log(name.value);
 
-  // const expression = {
-  //   name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
-  //   lastName: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
-  //   email: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, //eslint-disable-line
-  //   phone: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/, //eslint-disable-line
-  //   password:
-  //     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-  //   // Minimum eight and maximum 10 characters, at least one uppercase letter, one lowercase letter, one number and one special character (*),
+  // const [user, setUser] = useState({
+  //   name1: name,
+  //   lastName1: lastName,
+  //   email1: email,
+  //   phone1: phone,
+  //   direction1: direction,
+  //   cedula1: cedula,
+  // });
+  // const { name1, lastName1, email1, phone1, direction1, cedula1 } = user;
+  const user = {
+    name: name.value,
+    lastName: lastName.value,
+    email: email.value,
+    phone: phone.value,
+    direction: direction.value,
+    cedula: cedula.value,
+  };
+  // const { name, lastName, email, phone, direction, cedula } = user;
+  console.log("name1 should be: ", user.name);
+
+  const expression = {
+    name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+    lastName: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+    email: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, //eslint-disable-line
+    phone: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/, //eslint-disable-line
+    // password:
+    //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    // Minimum eight and maximum 10 characters, at least one uppercase letter, one lowercase letter, one number and one special character (*),
+  };
+
+  // const handleOnChange = (e) => {
+  //   // e.preventDefault();
+
+  //   setUser({
+  //     ...user,
+  //       [e.target.name]: e.target.value,
+  //     })
   // };
-
-  const handleOnChange = (e) => {
-    // e.preventDefault();
-
-    setUser({
-      ...user,
-      [e.target.name]: e.target.value,
-    });
-    setErrors(
-      validation({
-        ...user,
-        [e.target.name]: e.target.value,
-      })
-    );
-  };
-  const handleSubmit = async (e) => {
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   console.log(user);
+  //   try {
+  //     if (
+  //       name.length !== 0 &&
+  //       lastName.length !== 0 &&
+  //       email.length !== 0 &&
+  //       phone.length !== 0 &&
+  //       cedula.length !== 0 &&
+  //       direction.length !== 0
+  //     ) {
+  //       await dispatch(registerUser(user));
+  //       await dispatch(currentUser(user.cedula));
+  //       setUser({
+  //         name: "",
+  //         lastName: "",
+  //         email: "",
+  //         phone: 0,
+  //         direction: "",
+  //         cedula: 0,
+  //       });
+  //       // navigate.push("/");
+  //     } else if (
+  //       name.length !== 0 &&
+  //       lastName.length !== 0 &&
+  //       email.length !== 0 &&
+  //       phone.length !== 0 &&
+  //       cedula.length !== 0 &&
+  //       direction.length !== 0
+  //     ) {
+  //       alert("Please fill all the fields");
+  //     }
+  //   } catch (error) {
+  //     console.log("Error to Create a User", error);
+  //   }
+  // };
+  async function handleSubmit(e) {
     e.preventDefault();
-    console.log(user);
-    try {
-      if (
-        name.length !== 0 &&
-        lastName.length !== 0 &&
-        email.length !== 0 &&
-        phone.length !== 0 &&
-        cedula.length !== 0 &&
-        direction.length !== 0
-      ) {
-        await dispatch(registerUser(user));
-        await dispatch(currentUser(user.cedula));
-        setUser({
-          name: "",
-          lastName: "",
-          email: "",
-          phone: 0,
-          direction: "",
-          cedula: 0,
-        });
-        navigate.push("/");
-      } else if (
-        name.length !== 0 &&
-        lastName.length !== 0 &&
-        email.length !== 0 &&
-        phone.length !== 0 &&
-        cedula.length !== 0 &&
-        direction.length !== 0
-      ) {
-        alert("Please fill all the fields");
-      }
-    } catch (error) {
-      console.log("Error to Create a User", error);
+    // setLoading(true);
+
+    if (
+      name.valid === "true"
+      // direction.valid === 'true' &&
+      // lastName.valid === "true" &&
+      // email.valid === "true" &&
+      // phone.valid === "true"
+      // password.valid === 'true' &&
+      // password2.value === password.value
+    ) {
+      // let type = await dispatch(registerUser(
+      //   e.target.name.value,
+      //   e.target.lastName.value,
+      //   e.target.email.value,
+      //   e.target.cedula.value,
+      //   e.target.phone.value,
+      //   e.target.direction.value,
+      //   // `${e.target.name.value} ${e.target.lastName.value}`
+      // ));
+      // let type = e.target.name.value
+      // console.log(type)
+      // console.log(user.cedula.value)
+      await dispatch(registerUser(user));
+      await dispatch(currentUser(cedula.value));
+
+      // if (typeof type == "string") {
+      //   alert(type);
+      //   // Swal.fire({
+      //   //   icon: "error",
+      //   //   title: "Oops...",
+      //   //   text: "Something went wrong!",
+      //   //   confirmButtonColor: "#10408F",
+      //   // });
+      //   // setLoading(false);
+      // } else {
+      //   navigate.push("/");
+      //   //window.location.reload();
+      // }
+    } else {
+      // Swal.fire({
+      //   icon: "question",
+      //   title: "Oops...",
+      //   text: "Complete all fields",
+      //   confirmButtonColor: "#10408F",
+      // });
+      alert("fill the blanks");
+      // setLoading(false);
     }
-  };
+  }
 
   return (
     <>
@@ -212,12 +277,72 @@ export default function Register() {
                     onSubmit={(e) => handleSubmit(e)}
                     className="ltn__form-box contact-form-box"
                   >
-                    <input
+                    <Input
+                      state={name}
+                      setState={setName}
+                      type="text"
+                      // label="First Name"
+                      placeholder="First Name"
+                      // value={name}
+                      name="name"
+                      error="Your first name cannot contain numbers or special characters"
+                      regularExpression={expression.name}
+                    />
+                    <Input
+                      state={lastName}
+                      setState={setLastName}
+                      name="lastName"
+                      type="text"
+                      // label="Last Name"
+                      placeholder="Last Name"
+                      error="Your last name cannot contain numbers or special characters"
+                      regularExpression={expression.lastName}
+                    />
+                    <Input
+                      state={email}
+                      setState={setEmail}
+                      name="email"
+                      type="email"
+                      // label="E-mail"
+                      placeholder="E-mail"
+                      error="Please enter a valid email"
+                      regularExpression={expression.email}
+                    />
+                    <Input
+                      state={phone}
+                      setState={setPhone}
+                      name="phone"
+                      type="number"
+                      // label="Phone"
+                      placeholder="Phone"
+                      error="Please enter a valid phone number"
+                      regularExpression={expression.phone}
+                    />
+                    <Input
+                      state={cedula}
+                      setState={setCedula}
+                      name="cedula"
+                      type="number"
+                      // label="Phone"
+                      placeholder="Cedula"
+                      error="Please enter a valid cedula"
+                      regularExpression={expression.cedula}
+                    />
+                    <Input
+                      state={direction}
+                      setState={setDirection}
+                      name="direction"
+                      type="text"
+                      // label="Phone"
+                      placeholder="Dirección"
+                      error="Please enter a valid direction"
+                      regularExpression={expression.direction}
+                    />
+                    {/* <input
                       type="text"
                       placeholder="Name"
                       value={name}
                       name="name"
-                      pattern="^[a-zA-ZÀ-ÿ\s]{1,40}$"
                       onChange={(e) => handleOnChange(e)}
                     />
                     <input
@@ -254,7 +379,7 @@ export default function Register() {
                       value={phone}
                       name="phone"
                       onChange={(e) => handleOnChange(e)}
-                    />
+                    /> */}
                     {/* <input
                       type="password"
                       name="password"
