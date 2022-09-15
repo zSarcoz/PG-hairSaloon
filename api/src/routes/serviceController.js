3ea43550c98fdb8c81365c84c9049e56e642e836
zSarcoz <cesar.cottua@gmail.com>
2022-09-15 13:30:57 -0400

    servicios ya se postean

--- api/src/routes/serviceController.js
+++ api/src/routes/serviceController.js
@@ -1,8 +1,10 @@
 const { Services} = require("../db.js");
 const { Router } = require("express");
+const allServices = require("../json/db_men.json")
+const allServicesW = require("../json/db_women.json")
 const router = Router();
 
-router.get("/", async (req, res, next) => {
+router.get("/allServices", async (req, res, next) => {
     try {
       let allServices = await Services.findAll();
       if (allServices.length) {
@@ -14,6 +16,65 @@ router.get("/", async (req, res, next) => {
       next(error.message);
     }
   });
+
+  router.post("/allServices/men", async(req, res, next) => {
+    let servicios = allServices.services.map((service) => service)
+    console.log(servicios)
+    try{
+      servicios.forEach((ser) => {
+        console.log(ser.price)
+        Services.findOrCreate({
+          where: {
+            name: ser.name,
+            subtipos: ser.subtipos,
+            prices: ser.price
+          }
+        })
+      })
+      res.status(200).json(servicios)
+      // return servicios
+    }catch (error) {
+      next(error.message);
+    }
+  });
+
+  router.post("/allServices/women", async(req, res, next) => {
+    let servicios = allServicesW.services.map((service) => service)
+    console.log(servicios)
+    try{
+      servicios.forEach((ser) => {
+        console.log(ser.price)
+        Services.findOrCreate({
+          where: {
+            name: ser.name,
+            subtipos: ser.subtipos,
+            prices: ser.price
+          }
+        })
+      })
+      res.status(200).json(servicios)
+      // return servicios
+    }catch (error) {
+      next(error.message);
+    }
+  });
+
+  // router.get("/allServices", async(req, res, next) => {
+  //   let servicios = allServices.map((service) => service)
+  //   console.log(servicios)
+  //   try{
+  //     let allServicios = await servicios.create({
+
+  //     })
+  //     if(servicios.length){
+  //       res.status(200).json(servicios)
+  //     } else {
+  //       res.status(404).send({ message: "Services not found" });
+  //     }
+  //   }catch (error) {
+  //     next(error.message);
+  //   }
+  // });
 
   router.post("/", async (req, res) => {
     const { name, prices, subtipos} =
