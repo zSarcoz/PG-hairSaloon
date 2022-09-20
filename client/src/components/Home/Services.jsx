2a0f0d256071411417f63602466cedbf3342b219
zSarcoz <cesar.cottua@gmail.com>
2022-09-20 16:32:09 -0400

    services filter

--- client/src/components/Home/Services.jsx
+++ client/src/components/Home/Services.jsx
@@ -1,29 +1,44 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { getServices, setServices } from "../../redux/actions";
-import ServicesCard from "./ServicesCard"
+import ServicesCard from "./ServicesCard";
+import s from "../styles/Services.module.css";
 
 export default function Services() {
-
   const allServices = useSelector((state) => state.services);
+  console.log("allServices", allServices)
   const dispatch = useDispatch();
   const navigate = useHistory();
 
-  console.log("allBarbers", allServices);
-
   useEffect(() => {
-    dispatch(setServices())
+    dispatch(setServices());
     dispatch(getServices());
   }, [dispatch]);
 
+  function handleOnclick(value){
+    if(value === "Consentidas"){
+      let mujeres = allServices.filter(service => service.sexo === "Consentidas")
+      console.log("Mujeres: ",mujeres)
+    }else{
+      let hombres = allServices.filter(service => service.sexo === "Consentidos")
+      console.log("Hombres: ",hombres)
+    }
+  }
+
   return (
-    <>
+    <div className={s.container}>
       {/* <div> */}
-      {allServices.map((project, index) => {
-            return <ServicesCard key={index} {...project} />;
-          })}
-      {/* </div> */}
-    </>
+      <div className={s.botones}>
+        <button className={s.btn} value="Consentidas" onClick={() => handleOnclick()}>Consentidas</button>
+        <button className={s.btn} value="Consentidos" onClick={() => handleOnclick()}>Consentidos</button>
+      </div>
+      <div className={s.cards}>
+        {allServices.map((project, index) => {
+          return <ServicesCard key={index} {...project} />;
+        })}
+        {/* </div> */}
+      </div>
+    </div>
   );
 }
--- client/src/components/Home/ServicesCard.jsx
+++ client/src/components/Home/ServicesCard.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import s from "../styles/ServicesCard.module.css";
 
-export default function ServicesCard({ name, prices, subtipos }) {
+export default function ServicesCard({ name, prices, subtipos, sexo }) {
   return (
-    <div className={s.serviceCard}>
+    <div className={sexo === "Consentidas" ? s.serviceCard : s.servicesCardMan}>
       <h1 className={s.name}>{name}</h1>
       <h3 className={s.price}>Desde: {prices[0]}$</h3>
       <h3 className={s.price}>Hasta: {prices[0]}$</h3>
+      <h3 className={s.price}>{subtipos.join(", ")}</h3>
       {/* <p className={s.description}>{description}</p> */}
     </div>
   );
