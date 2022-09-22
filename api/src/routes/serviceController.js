d2f51338a04bea1c0624e662ae3ca55ad812d634
zSarcoz <cesar.cottua@gmail.com>
2022-09-22 14:19:47 -0400

    Icons

--- api/src/routes/serviceController.js
+++ api/src/routes/serviceController.js
@@ -28,7 +28,8 @@ router.get("/allServices", async (req, res, next) => {
             name: ser.name,
             subtipos: ser.subtipos,
             prices: ser.price,
-            sexo: ser.sexo
+            sexo: ser.sexo,
+            image: ser.image
           }
         })
       })
@@ -38,44 +39,6 @@ router.get("/allServices", async (req, res, next) => {
       next(error.message);
     }
   });
-
-  router.post("/allServices/women", async(req, res, next) => {
-    let servicios = allServicesW.services.map((service) => service)
-    console.log(servicios)
-    try{
-      servicios.forEach((ser) => { 
-        console.log(ser.price)
-        Services.findOrCreate({
-          where: {
-            name: ser.name,
-            subtipos: ser.subtipos,
-            prices: ser.price
-          }
-        })
-      })
-      res.status(200).json(servicios)
-      // return servicios
-    }catch (error) {
-      next(error.message);
-    }
-  });
-
-  // router.get("/allServices", async(req, res, next) => {
-  //   let servicios = allServices.map((service) => service)
-  //   console.log(servicios)
-  //   try{
-  //     let allServicios = await servicios.create({
-
-  //     })
-  //     if(servicios.length){
-  //       res.status(200).json(servicios)
-  //     } else {
-  //       res.status(404).send({ message: "Services not found" });
-  //     }
-  //   }catch (error) {
-  //     next(error.message);
-  //   }
-  // });
 
   router.post("/", async (req, res) => {
     const { name, prices, subtipos} =
--- client/src/components/Home/ServicesCard.jsx
+++ client/src/components/Home/ServicesCard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import s from "../styles/ServicesCard.module.css";
 
-export default function ServicesCard({ name, prices, subtipos, sexo }) {
+export default function ServicesCard({ name, prices, subtipos, sexo, image }) {
+  console.log(image);
   // console.log(name, prices, subtipos, sexo)
   return (
-    <div className={sexo === "Consentidas" ? s.serviceCard : s.servicesCardMan}>
-      <h1 className={s.name}>{name}</h1>
-      <h3 className={s.price}>Desde: {prices[0]}$</h3>
-      <h3 className={s.price}>Hasta: {prices[0]}$</h3>
-      <h3 className={s.price}>{subtipos.join(", ")}</h3>
-      {/* <p className={s.description}>{description}</p> */}
-    </div>
+    <>
+      <div
+        className={sexo === "Consentidas" ? s.serviceCard : s.servicesCardMan}
+      >
+        <h1 className={s.name}>{name}</h1>
+        <h3 className={s.price}>Desde: {prices[0]}$</h3>
+        <img src={image} alt="icon" />
+      </div>
+    </>
   );
 }
