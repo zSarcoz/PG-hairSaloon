d361d97aa86fd5e039146e3734804c9f69b3621e
Sebas_montilla <sebastianam127@gmail.com>
2022-09-23 00:20:33 -0400

    Services almost done

--- client/src/components/Home/ServicesCard.jsx
+++ client/src/components/Home/ServicesCard.jsx
@@ -6,13 +6,14 @@ export default function ServicesCard({ name, prices, subtipos, sexo, image }) {
   // console.log(name, prices, subtipos, sexo)
   return (
     <>
-      <div
+      
+      <button
         className={sexo === "Consentidas" ? s.serviceCard : s.servicesCardMan}
       >
         <img className={s.logosServices} src={image} alt="icon" />
         <h1 className={sexo === "Consentidas" ? s.name : s.nameM}>{name}</h1>
         <h3 className={sexo === "Consentidas" ? s.price : s.priceM}>Desde: ${prices[0]}</h3>
-      </div>
+      </button>
     </>
   );
 }
